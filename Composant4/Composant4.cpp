a45f44bac3806224836ced00584d999ee2a58b93
dorraJrad <dorra.jradd@gmail.com>
2016-01-22 14:21:31 +0100

    Création du composant4, avec le composant1 en commentaire et le composant 4 qui le substitue

---
+++ Composant4/Composant4.cpp
@@ -0,0 +1,39 @@
+
+#include "Composant1.h"
+#include "Composant3.h"
+#include "Composant4Version.h"
+#include <string>
+
+//#define _CRT_SECURE_NO_WARNINGS
+
+// declaration des fonctions internes
+int addition_interne(int a1, int a2);
+
+
+
+int composant1(int p1, int p2)
+{
+	return composant3(p1, p2);
+}
+
+int addition_interne(int a1, int a2)
+{
+	return a1 + a2;
+}
+
+char * getComposant1Version()
+{
+	char* lib1 = "Composant 1 version ";
+	char* version1 = COMPOSANT_VERSION_STR;
+	char* lib2 = "\n ";
+	char * version3 = getComposant3Version();
+
+	char *  version = (char*)malloc(80 + strlen(version1) + strlen(version3));
+
+	strcpy(version, lib1);
+	strcat(version, version1);
+	strcat(version, lib2);
+	strcat(version, version3);
+
+	return version;
+}
