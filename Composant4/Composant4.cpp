6a9d32982a242626035e5923401256193ebe4ad0
dorraJrad <dorra.jradd@gmail.com>
2016-01-25 09:20:57 +0100

    Modifications requises dans la derniere question meme interface que composant1

--- Composant4/Composant4.cpp
+++ Composant4/Composant4.cpp
@@ -1,7 +1,6 @@
-
-#include "Composant1.h"
-#include "Composant3.h"
+#include "Composant4.h"
 #include "Composant4Version.h"
+
 #include <string>
 
 //#define _CRT_SECURE_NO_WARNINGS
@@ -13,27 +12,15 @@ int addition_interne(int a1, int a2);
 
 int composant1(int p1, int p2)
 {
-	return composant3(p1, p2);
+	return addition_interne(p1, p2);
 }
 
 int addition_interne(int a1, int a2)
 {
-	return a1 + a2;
+	return a1 + a2+10;
 }
 
 char * getComposant1Version()
 {
-	char* lib1 = "Composant 1 version ";
-	char* version1 = COMPOSANT_VERSION_STR;
-	char* lib2 = "\n ";
-	char * version3 = getComposant3Version();
-
-	char *  version = (char*)malloc(80 + strlen(version1) + strlen(version3));
-
-	strcpy(version, lib1);
-	strcat(version, version1);
-	strcat(version, lib2);
-	strcat(version, version3);
-
-	return version;
+	return "Composant 1 version " COMPOSANT_VERSION_STR;
 }
